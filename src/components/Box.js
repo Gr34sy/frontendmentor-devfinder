f048aebfaa9c99900e168018733267efbb418cd5
Gr34sy <slupek3@poczta.fm>
2024-04-13 20:13:35 +0200

    started to adding html to box component

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,7 +1,13 @@
 export function Box() {
   return (
     <div className="bg-white-200 dark:bg-slate-800">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi perspiciatis nemo deleniti saepe laborum, dicta numquam facilis explicabo eius voluptate dolore delectus a ut similique dignissimos quis quasi voluptatum dolores ea maiores est blanditiis. Itaque distinctio nulla accusamus ipsum dolorem!
+      <img src="/" alt="Github user avatar"/>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
