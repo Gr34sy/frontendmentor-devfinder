82d42d96d5284207e461865037c2cc6728ca4495
Gr34sy <slupek3@poczta.fm>
2024-04-15 19:23:02 +0200

    finished last container in box component

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,4 +1,9 @@
 import avatar from "../assets/DummyAvatar.jpg";
+import iconCompany from "../assets/icon-company.svg";
+import iconTwitter from "../assets/icon-twitter.svg";
+import iconLocation from "../assets/icon-location.svg";
+import iconWebsite from "../assets/icon-website.svg";
+import { NotAvialable } from "./NotAvialable";
 
 export function Box() {
   return (
@@ -11,7 +16,7 @@ export function Box() {
         />
       </div>
 
-      <div className="grid gap-5">
+      <div className="grid gap-6">
         <div className="grid grid-cols-auto1fr--mobile">
           <div className="avatar-div--mobile">
             <img
@@ -24,41 +29,64 @@ export function Box() {
           <div className="grid header-title">
             <div>
               <h2 className="font-bold text-3xl">The User</h2>
-              <p>@user</p>
+              <p className="text-blue-600">@user</p>
             </div>
 
-            <p>Joined D M Y</p>
+            <p className="text-blue-700 dark:text-white-100">Joined D M Y</p>
           </div>
         </div>
 
-        <p>
+        <p className="text-blue-800 dark:text-white-100">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit.
           Necessitatibus reprehenderit voluptas minima tempora quis, sequi esse,
           non amet adipisci enim quas odio sunt assumenda quaerat ab atque
           aliquid nostrum praesentium?
         </p>
 
-        <div className="flex justify-between align-center rounded-lg bg-white-400 p-5 text-left">
+        <div className="flex justify-between align-center rounded-lg bg-white-400 dark:bg-slate-900 p-5 text-left">
           <div>
-            <p className="text-blue-800 text-lg">Repos</p>
+            <p className="text-blue-800 text-lg dark:text-white-100">Repos</p>
             <p className="font-bold text-2xl">8</p>
           </div>
           <div>
-            <p className="text-blue-800 text-lg">Followers</p>
+            <p className="text-blue-800 text-lg dark:text-white-100">
+              Followers
+            </p>
             <p className="font-bold text-2xl">9999</p>
           </div>
           <div>
-            <p className="text-blue-800 text-lg">Following</p>
+            <p className="text-blue-800 text-lg dark:text-white-100">
+              Following
+            </p>
             <p className="font-bold text-2xl">150</p>
           </div>
         </div>
 
-        <div>
-          <div>
-            <p></p>
+        <div className="flex gap-12 text-blue-800 dark:text-white-100">
+          <div className="grid gap-2">
+            <div className="flex gap-2 items-center">
+              <img src={iconLocation} alt="location icon" />
+              <p>San Francisco</p>
+            </div>
+
+            <div className="flex gap-2 items-center">
+              <img src={iconWebsite} alt="link icon" />
+              <a href="/" className="underline">
+                https://github.blog
+              </a>
+            </div>
           </div>
-          <div>
-            <p></p>
+
+          <div className="grid gap-2">
+            <div className="flex gap-2 items-center">
+              <img src={iconTwitter} alt="twitter icon" />
+              <NotAvialable />
+            </div>
+
+            <div className="flex gap-2 items-center">
+              <img src={iconCompany} alt="icon of a building" />
+              <p>@github</p>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/NotAvialable.js
@@ -0,0 +1,7 @@
+export function NotAvialable(){
+    return (
+        <p className="text-slate-300 dark:text-slate-600">
+            Not Avialable
+        </p>
+    )
+}
