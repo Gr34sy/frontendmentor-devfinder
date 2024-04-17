29f0d004de39932b2cac17e1a16cd3917982f1b1
Gr34sy <slupek3@poczta.fm>
2024-04-17 16:23:41 +0200

    changes in app and searchbar, added data fetching function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import "./App.css";
+
 import { Box } from "./components/Box";
 import { Layout } from "./components/Layout";
-import { useState } from "react";
 import { SearchBar } from "./components/SearchBar";
 import { Header } from "./components/Header";
 
+import { useState } from "react";
+
 function App() {
   const [darkmode, setDarkmode] = useState(false);
-  const [name, setName] = useState("");
+  const [user, setUser] = useState(true);
+
+  async function searchFor(user) {
+    fetch(`https://api.github.com/users/${user}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message === "Not Found") {
+          setUser("no results");
+
+          setTimeout(() => {
+            setUser(false);
+          }, 2000);
+        } else {
+          setUser(data);
+          console.log(data);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
   function toggleMode() {
     setDarkmode((prevMode) => !prevMode);
@@ -16,9 +38,12 @@ function App() {
   return (
     <div className={darkmode ? "App dark" : "App"}>
       <Layout>
-        <Header onClick={toggleMode} darkmode={darkmode}/>
-        <SearchBar value={name} onChange={setName}/>
-        <Box />
+        <Header onClick={toggleMode} darkmode={darkmode} />
+        <SearchBar
+          noResults={user === "no results" ? true : false}
+          onClick={searchFor}
+        />
+        {user && <Box user={user} />}
       </Layout>
     </div>
   );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,33 @@
 import searchIcon from "../assets/icon-search.svg";
+import { useState } from "react";
 
-export function SearchBar({value, displayNoResults, onChange}) {
+export function SearchBar({ noResults, onClick }) {
+  const [inputValue, setInputValue] = useState("");
 
-    function handleChange(e){
-        if(typeof onChange === 'function'){
-            onChange(e.target.value);
-        }
+  function handleClick() {
+    if (typeof onClick === "function") {
+      onClick(inputValue);
     }
+  }
 
   return (
     <div className="pl-2 sm:pl-4 py-2 pr-2 bg-white-200 dark:bg-slate-800 rounded-lg shadow-sm shadow-blue-700 dark:shadow-none grid sm:gap-3 grid--searchbar">
-      <label htmlFor="name" className="grid gap-1 sm:gap-2  items-center hover:cursor-pointer">
-        <img src={searchIcon} alt="icon of magnifying glass"/>
-        <input id="name" type="text" className="bg-white-200 dark:bg-slate-800 sm:text-lg self-stretch dark:outline-none active:outline-blue-400 focus:outline-blue-400" placeholder="Search for github username..." value={value} onChange={handleChange}/>
-        {displayNoResults && <span>No Results</span>}
+      <label
+        htmlFor="name"
+        className="grid gap-1 sm:gap-2  items-center hover:cursor-pointer"
+      >
+        <img src={searchIcon} alt="icon of magnifying glass" />
+        <input
+          id="name"
+          type="text"
+          className="bg-white-200 dark:bg-slate-800 sm:text-lg self-stretch dark:outline-none active:outline-blue-400 focus:outline-blue-400"
+          placeholder="Search for github username..."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        {noResults && <span>No Results</span>}
       </label>
-      <button className="py-3 px-1 sm:px-5 bg-blue-600 hover:bg-blue-400 border-none text-white-100 sm:text-lg rounded-lg">
+      <button className="py-3 px-1 sm:px-5 bg-blue-600 hover:bg-blue-400 border-none text-white-100 sm:text-lg rounded-lg" onClick={handleClick}>
         Search
       </button>
     </div>
